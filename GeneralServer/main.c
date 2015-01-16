bd05c9cdf1c5af1cdb76cbeb997b256e71627cd9
Alec Zadikian <alecz@berkeley.edu>
2015-01-16 12:48:06 -0800

    Initial Commit

---
+++ GeneralServer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  GeneralServer
+//
+//  Created by Alec Zadikian on 1/16/15.
+//  Copyright (c) 2015 Alec Zadikian. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
